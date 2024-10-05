fa4bf692a01d8d02f6bd842498e5017b7149ddb5
ishita3333 <kritisunilrao6@gmail.com>
2024-10-05 12:57:51 +0530

    BB C programing

--- C/Easy/2.c
+++ C/Easy/2.c
@@ -1,7 +1,8 @@
+#include <stdio.h>
 int main() {
     int a = 10;
-    double b = 5.5;
-    int c = a + b;
-    printf("The sum of a and b is %d", c);
+    float b = 5.5;
+    float c = a + b;
+    printf("The sum of a and b is %f", c);
     return 0;
 } 
