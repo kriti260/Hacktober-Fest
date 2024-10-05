390293724d6fb25359e1feb48878240ba514b0dc
ishita3333 <kritisunilrao6@gmail.com>
2024-10-05 13:46:02 +0530

    BB C programing 24.C

--- C/Easy/24.c
+++ C/Easy/24.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-#include<conio.h>
 int main() {
     int a;
 
     printf("Enter your age: ");
     scanf("%d", &a);
 
-    if (a >= 18) {
-        printf("You are eligible for a license.\n");
-    } else {
-        printf("Sorry! You are not eligible for a license.\n");
+    if (a >= 18) 
+    {
+        printf("You are eligible for a license.\n",a);
+    } 
+    else
+     {
+        printf("Sorry! You are not eligible for a license.\n",a);
     }
 
     return 0;
