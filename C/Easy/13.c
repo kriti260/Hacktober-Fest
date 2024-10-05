5ff1ce1b01c9c38d2c03743b4a99e80baba241c9
ishita3333 <kritisunilrao6@gmail.com>
2024-10-05 13:35:24 +0530

    BB C programing 13.C

--- C/Easy/13.c
+++ C/Easy/13.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main()
+ {
     int a = 10;
-    if (a = 5) {
+    if (a == 5)
+     {
         printf("a is 5");
-    } else {
+    }
+     else 
+    {
         printf("a is not 5");
     }
     return 0;
