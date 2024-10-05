83edf36f2c5fc280d2deeb1f567c48a5a8be8c4e
ishita3333 <kritisunilrao6@gmail.com>
2024-10-05 13:21:34 +0530

    BB C programing 9.C

--- C/Easy/9.c
+++ C/Easy/9.c
@@ -3,9 +3,8 @@
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
     int sum = 0;
-    for (int i = 0; i <= 5; i++) {
-        sum += arr[i];
-    }
+    for (int i = 1; i <= 5; i++)
+    sum += arr[i];
     printf("Sum of array elements: %d\n", sum);
     return 0;
 }
